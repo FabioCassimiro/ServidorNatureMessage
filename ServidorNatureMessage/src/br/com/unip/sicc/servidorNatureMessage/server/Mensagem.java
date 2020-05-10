256c4e77089bf7ef17743d0ee2b8614c6e7b708b
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-05-10 14:49:12 -0300

    edição

--- ServidorNatureMessage/src/br/com/unip/sicc/servidorNatureMessage/server/Mensagem.java
+++ ServidorNatureMessage/src/br/com/unip/sicc/servidorNatureMessage/server/Mensagem.java
@@ -54,9 +54,8 @@ public class Mensagem {
             } else {
                 montaMensagem += mensagem[0] + "  " + mensagem[1] + "  " + mensagem[2];
             }
-            clientes.get(i).println(montaMensagem);
+            clientes.get(i).println(montaMensagem + "**");
             clientes.get(i).flush();
-
         }
 
     }
